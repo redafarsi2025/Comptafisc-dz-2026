78eb668691b48dd65055ab2328f6b4802b75eeb2
reda farsi <mohamedredhafarsi@gmail.com>
2026-03-18 07:58:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2827725920-104c2",
+  "appId": "1:149089462735:web:7205df585a403a9c434aba",
+  "apiKey": "AIzaSyBFIGa5FLcpuVvdmY8CpMshJX0ZRV3n-D8",
+  "authDomain": "studio-2827725920-104c2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "149089462735"
+};
